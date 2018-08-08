984dc052c1163bcd9dcafe768ebd2efc85337807
Tira <cosmicsam1010@yahoo.com>
2018-08-08 12:37:18 -0700

    Add files via upload
    Has the camera follow the player

---
+++ CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public MovePlayer movePlayer;
+    public bool isFollowing;
+    public float xOffset;
+    public float yOffset;
+
+	// Use this for initialization
+	void Start () {
+        movePlayer = FindObjectOfType<MovePlayer>();
+
+        isFollowing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isFollowing)
+            transform.position = new Vector3(movePlayer.transform.position.x + xOffset , movePlayer.transform.position.y + yOffset + 2, transform.position.z);
+	}
+}
