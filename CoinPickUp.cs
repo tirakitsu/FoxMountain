1bccf0720deab3bf763beb8c49e0cc08b99be14c
Tira <cosmicsam1010@yahoo.com>
2018-08-08 12:38:25 -0700

    Add files via upload
    Deletes coin and adds a point to player information

---
+++ CoinPickUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickUp : MonoBehaviour {
+
+    public int pointsToAdd;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<MovePlayer>() == null)
+            return;
+        ScoreManager.AddPoints(pointsToAdd);
+
+        Destroy (gameObject);
+
+    }
+
+}
